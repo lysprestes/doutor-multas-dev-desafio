dbadb48b39fdbd5f1dced887e80e61646079ae46
Lys Prestes <lysprestes@gmail.com>
2022-03-14 23:23:11 -0400

    Style guides and accordion setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import Carros from './components/Carros'
-import 'tw-elements';
+import Accordion from './components/Accordion'
+import Logo from './logotipo.png'
+import 'tw-elements'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
+      <header class="text-gray-600 body-font stiky">
+        <div class="container mx-auto flex flex-wrap justify-between p-5 flex-col md:flex-row items-center">
+          
+          <a href="https://doutormultas.com.br/" target="_blank">
+            <img src={Logo} alt="" srcset="" className="max-h-12" />
+          </a>
+          <a
+            href="https://doutormultas.com.br/consulta-gratis/?utm_content=popup-hellobar"
+            target="_blank">
+            <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Você foi multado?
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                class="w-4 h-4 ml-1"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
+          </a>
+        </div>
+      </header>
+      <Accordion/>
     </div>
   )
 }
---
+++ src/components/Accordion.jsx
@@ -0,0 +1,159 @@
+export default function Accordion() {
+  return (
+    <div>
+      <div className="min-h-screen bg-gray-200 py-6">
+        <div className="px-6 pt-10 pb-8 bg-white shadow-xl ring-1 ring-gray-900/5 sm:max-w-lg sm:mx-auto sm:rounded-lg sm:px-10">
+          <div className="max-w-md mx-auto">
+            <div className="divide-y divide-gray-300/50">
+              <div className="py-5 text-base space-y-6 text-gray-700">
+                <p>Procure seu veículo pelo modelo:</p>
+              </div>
+            </div>
+            <div class="accordion" id="accordionExample">
+              <div class="accordion-item bg-white border border-gray-200">
+                <h2 class="accordion-header mb-0" id="headingOne">
+                  <button
+                    class="
+                      accordion-button
+                      relative
+                      flex
+                      items-center
+                      w-full
+                      py-4
+                      px-5
+                      text-base text-gray-800 text-left
+                      bg-white
+                      border-0
+                      rounded-none
+                      transition
+                      focus:outline-none
+                    "
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseOne"
+                    aria-expanded="true"
+                    aria-controls="collapseOne"
+                  >
+                    Accordion Item #1
+                  </button>
+                </h2>
+                <div
+                  id="collapseOne"
+                  class="accordion-collapse collapse show"
+                  aria-labelledby="headingOne"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body py-4 px-5">
+                    <strong>This is the first item's accordion body.</strong> It
+                    is shown by default, until the collapse plugin adds the
+                    appropriate classes that we use to style each element. These
+                    classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any
+                    of this with custom CSS or overriding our default variables.
+                    It's also worth noting that just about any HTML can go
+                    within the <code>.accordion-body</code>, though the
+                    transition does limit overflow.
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item bg-white border border-gray-200">
+                <h2 class="accordion-header mb-0" id="headingTwo">
+                  <button
+                    class="
+                      accordion-button
+                      collapsed
+                      relative
+                      flex
+                      items-center
+                      w-full
+                      py-4
+                      px-5
+                      text-base text-gray-800 text-left
+                      bg-white
+                      border-0
+                      rounded-none
+                      transition
+                      focus:outline-none
+                    "
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseTwo"
+                    aria-expanded="false"
+                    aria-controls="collapseTwo"
+                  >
+                    Accordion Item #2
+                  </button>
+                </h2>
+                <div
+                  id="collapseTwo"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingTwo"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body py-4 px-5">
+                    <strong>This is the second item's accordion body.</strong>{' '}
+                    It is hidden by default, until the collapse plugin adds the
+                    appropriate classes that we use to style each element. These
+                    classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any
+                    of this with custom CSS or overriding our default variables.
+                    It's also worth noting that just about any HTML can go
+                    within the <code>.accordion-body</code>, though the
+                    transition does limit overflow.
+                  </div>
+                </div>
+              </div>
+              <div class="accordion-item bg-white border border-gray-200">
+                <h2 class="accordion-header mb-0" id="headingThree">
+                  <button
+                    class="
+                      accordion-button
+                      collapsed
+                      relative
+                      flex
+                      items-center
+                      w-full
+                      py-4
+                      px-5
+                      text-base text-gray-800 text-left
+                      bg-white
+                      border-0
+                      rounded-none
+                      transition
+                      focus:outline-none
+                    "
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#collapseThree"
+                    aria-expanded="false"
+                    aria-controls="collapseThree"
+                  >
+                    Accordion Item #3
+                  </button>
+                </h2>
+                <div
+                  id="collapseThree"
+                  class="accordion-collapse collapse"
+                  aria-labelledby="headingThree"
+                  data-bs-parent="#accordionExample"
+                >
+                  <div class="accordion-body py-4 px-5">
+                    <strong>This is the third item's accordion body.</strong> It
+                    is hidden by default, until the collapse plugin adds the
+                    appropriate classes that we use to style each element. These
+                    classes control the overall appearance, as well as the
+                    showing and hiding via CSS transitions. You can modify any
+                    of this with custom CSS or overriding our default variables.
+                    It's also worth noting that just about any HTML can go
+                    within the <code>.accordion-body</code>, though the
+                    transition does limit overflow.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
